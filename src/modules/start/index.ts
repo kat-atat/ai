45a23010991a514d9978390ce162265e8692ade4
Leies <lei@eizi2002.skr.jp>
2019-11-08 23:09:51 +0900

    どうかな～

---
+++ src/modules/start/index.ts
@@ -0,0 +1,6 @@
+import autobind from 'autobind-decorator';
+import Module from '../../module';
+import Message from '../../message';
+
+this.ai.post({ text: `...はっ！？` });
+this.ai.post({ text: `寝てた...。` });
