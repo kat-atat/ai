be4864f5877c8b021503572ca7ab006d124f1681
Leies <lei@eizi2002.skr.jp>
2019-11-14 19:41:58 +0900

    カレーメシというかップメシなのだわ

--- src/modules/timer/index.ts
+++ src/modules/timer/index.ts
@@ -41,7 +41,7 @@ export default class extends Module {
 			return true;
 		}
 
-		const pre = time == 300 * 1000 ? 'またカレーメシ？' : '';
+		const pre = time == 300 * 1000 ? 'またカップメシ？' : '';
 
 		msg.reply(pre + serifs.timer.set);
 
